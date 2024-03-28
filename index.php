e21d8524a1cdc462c857e2213c598681585eea86
Okslineid <150103646+OkslineItsFine@users.noreply.github.com>
2024-03-28 09:00:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+1.<?php
+for($i=1; $i<11; $i++){
+	echo $i;
+    echo "<br>";
+ }
+?>
+
+2.<?php
+for($i=0; $i<11; $i++){
+	echo $i*2;
+    echo "<br>";
+ }
+?>
+
+3.<?php
+for($i=1; $i<7; $i++){
+    echo "<br>";
+    for($j=1; $j<$i; $j++) {
+    	echo $j;
+   }
+ }
+?>
+
+4.<?php
+for($i=1; $i<7; $i++){
+    echo "<br>";
+    for($j=1; $j<$i; $j++) {
+    	echo "*ss";
+   }
+ }
+?>
